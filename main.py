9841f2d50a3f0259d20ed4e51b3b57c5e4ba4dcb
superupperzhuang <2035548788@qq.com>
2025-12-23 14:01:34 +0800

    修改代码，实现作业要求全部功能

--- main.py
+++ main.py
@@ -17,17 +17,19 @@ if __name__=='__main__':
     while True:
         try:
             query = input("请输入查询：").strip()
-            if query.lower() in ['退出', 'quit', 'exit']:
+
+            if query.lower() in ['退出', 'quit', 'exit','再见']:
                 print("感谢使用书法咨询系统！")
                 break
-
-            if query.lower() in ['你好']:
-                print(ds.deepseekchat(query))
-
-            else:
-                result = dsl.parse(ds.deepseekchat(query))
+            response = ds.deepseekchat(query)
+            if response.startswith(('查询','查找','搜索','找','请问','我想知道','了解','显示','展示')):
+                result = dsl.parse(ds.deepseekchat(response))
                 print(f"{result}")
                 print("-" * 50)
+
+            else:
+                print(response)
+
         except KeyboardInterrupt:
             print("\n感谢使用书法咨询系统！")
             break
